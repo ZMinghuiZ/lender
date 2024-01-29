4db3010d8ca95481e3f2942038ba57580775c9c9
ZMinghuiZ <zmh992545@gmail.com>
2024-01-29 21:43:16 +0800

    Add loan file
    modify the code using OOP standards in lecture

---
+++ src/main/java/Loan.java
@@ -0,0 +1,55 @@
+public class Loan {
+    private int amount;
+    private String borrower;
+    private boolean isPaid;
+    private double interestRate;
+    private String contactNumber;
+
+    public Loan(int amount, String borrower, double interestRate, String contactNumber) {
+        this.amount = amount;
+        this.borrower = borrower;
+        this.interestRate = interestRate;
+        this.contactNumber = contactNumber;
+        this.isPaid = false;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
